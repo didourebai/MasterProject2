e5c3807f88bfe3472ecf8686a75f0e0d015eb582
REBAI HAmida <didourebai@gmail.com>
2018-01-22 13:08:39 +0100

    add new porject

---
+++ MasterProject/MasterProject/Views/MasterPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MasterProject.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MasterPage : MasterDetailPage
+    {
+		public MasterPage ()
+		{
+			InitializeComponent ();
+            Detail = new NavigationPage(new Page1());
+
+            //Summary:
+            //Gets or sets a value that indicates whether or not the visual element that is represented by the Xamarin.Forms.MasterDetailPage.Master property is presented to the user.
+
+            //    Remarks:
+            //Setting this property causes the Xamarin.Forms.MasterDetailPage.IsPresentedChanged event to be raised.
+            //We initialize it to false
+            IsPresented = false;
+		}
+
+
+        void goToPage1_Clicked(object sender, System.EventArgs e)
+        {
+            //We will display the first page
+            Detail = new NavigationPage(new Page1());
+            IsPresented = false;
+
+        }
+
+        void goToPage2_Clicked(object sender, System.EventArgs e)
+        {
+            //We will display the second page
+            Detail = new NavigationPage(new Page2());
+            IsPresented = false;
+
+        }
+
+        void goToPage3_Clicked(object sender, System.EventArgs e)
+        {
+            //We will display the third page
+            Detail = new NavigationPage(new Page3());
+            IsPresented = false;
+
+        }
+    }
+}
